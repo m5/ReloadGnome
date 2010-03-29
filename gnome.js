34ffe4400906e6ad9c68fe8b94a9035039cf8963
m5 <mfivecoa@purdue.edu>
2010-03-29 02:16:19 -0400

    refresh if touched now functional

--- gnome.js
+++ gnome.js
@@ -2,28 +2,39 @@ var gnome = {
   heard_user : new Date().getTime(), //when the gnome last heard the mouse
   timidity : 600,               //measured in seconds
   home_page : window.location,
+  heard_user_at : new Date().getTime(),
+  refreshed_page_at : new Date().getTime(),
+  
 
   hears_user : function(e){
-    gnome.heard_user = new Date().getTime();
+    gnome.heard_user_at = new Date().getTime();
   },
 
   peek : function(){
-    if (not gnome.heard_user){
-      return false;
-    }
-    safety = (new Date().getTime() - gnome.heard_user)/1000;
-    if (safety > gnome.timidity){
+    var page_touched = gnome.heard_user_at - gnome.refreshed_page_at > 1000;
+    var safety = (new Date().getTime() - gnome.heard_user_at)/1000;
+    console.log( page_touched );
+    console.log( gnome.heard_user_at - gnome.refreshed_page_at ); 
+    
+    if (page_touched && safety > gnome.timidity){
       gnome.reset_page();
     }else{
-      setInterval(gnome.peek, (gnome.timidity - safety)*1000);
+      if ( ! page_touched ){
+        var interval = gnome.timidity * 1000;
+      }else{
+      var interval = (gnome.timidity - safety) * 1000;
+      }
+      setInterval(gnome.peek, interval);
     }
-  },
 
+  },
+ 
   reset_page : function(){
     window.location = gnome.home_page;
-    gnome.heard_user = false;
+    gnome.refreshed_page_at = new Date().getTime()
+    gnome.heard_user_at = new Date().getTime()
   },
-};
+}
 
 window.addEventListener("keyDown",gnome.hears_user);
 window.addEventListener("mousemove",gnome.hears_user); //the gnome has very good ears
