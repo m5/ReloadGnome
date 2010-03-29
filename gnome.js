806ff901e5beea25ed679f39a3f98d510ee14b61
m5 <mfivecoa@purdue.edu>
2010-03-29 01:36:13 -0400

    only reloads if page was touched

--- gnome.js
+++ gnome.js
@@ -1,6 +1,6 @@
 var gnome = {
   heard_user : new Date().getTime(), //when the gnome last heard the mouse
-  timidity : 600, //measured in seconds
+  timidity : 600,               //measured in seconds
   home_page : window.location,
 
   hears_user : function(e){
@@ -8,6 +8,9 @@ var gnome = {
   },
 
   peek : function(){
+    if (not gnome.heard_user){
+      return false;
+    }
     safety = (new Date().getTime() - gnome.heard_user)/1000;
     if (safety > gnome.timidity){
       gnome.reset_page();
@@ -18,15 +21,18 @@ var gnome = {
 
   reset_page : function(){
     window.location = gnome.home_page;
-    gnome.hears_user();
+    gnome.heard_user = false;
   },
 };
 
 window.addEventListener("keyDown",gnome.hears_user);
 window.addEventListener("mousemove",gnome.hears_user); //the gnome has very good ears
 
-chrome.extension.sendRequest({type: "config", keys: 'timeout,refresh_to_page'}, function(response) {
-  gnome.timidity = response.timeout || gnome.timidity;
-  gnome.home_page = response.refresh_to_page || gnome.home_page;
-  gnome.peek();
-  });
+chrome.extension.sendRequest(
+    {type: "config",
+     keys: 'timeout,refresh_to_page'},
+    function(response) {
+      gnome.timidity  = response.timeout || gnome.timidity;
+      gnome.home_page = response.refresh_to_page || gnome.home_page;
+      gnome.peek();
+    });
